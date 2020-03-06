fbdab78fc10dc0f84d53b3dcd197df62c4cd57ed
akshlack <akshlack@github.com>
2020-03-06 21:23:34 +0300

    variables

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 alert('Hello, alert!');
 console.log('Hello console!');
+
+// Single line comment
+/*
+Multiline  comment
+*/
+
+// variables
+
+var firstname;
+var _lastname;
+var $patronymic;
+
+var camelCase;
+var bottleOfWater;
+
+// do not use transliterate butylkavody
