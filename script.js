4ef2afddc28dc5e8b801b70467db3470f9968e72
akshlack <akshlack@github.com>
2020-03-06 21:39:29 +0300

    Scalar data types

--- script.js
+++ script.js
@@ -16,3 +16,53 @@ var camelCase;
 var bottleOfWater;
 
 // do not use transliterate butylkavody
+
+// Five primitive types (scalar):
+var num = 5;
+var float = 5.5;
+
+// num and float both have a type 'number'
+console.log(typeof num);
+console.log(typeof float);
+
+
+
+// string
+var str1 = 'Hello, string variable!';
+var str2 = "Hello, string variable!";
+console.log(str1);
+console.log(str2);
+
+var str3 = 'Hello, string variable in "quotes!"';
+var str4 = "Hello, string variable in 'quotes!'";
+var str5 = "Hello, string variable in \"quotes!\"";
+
+console.log(str3);
+console.log(str4);
+console.log(str5);
+
+
+var bool = true;
+var lie = false;
+
+console.log(0 > 2);
+console.log(0< 2);
+
+
+
+
+var und; // undefinded
+var donutHole = null;
+
+// Structural object types
+var obj = {};
+var arr = [];
+
+// Get type of variable
+console.log(num);
+console.log(typeof bool);
+
+
+
+
+
